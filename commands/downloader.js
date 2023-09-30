6cc14ca551d535d5e859e646f52daaa49e90c25e
QUEENNICKYMDv1 <143050358+QUEENNICKYMDv1@users.noreply.github.com>
2023-09-30 09:00:51 +0530

    Update downloader.js

--- commands/downloader.js
+++ commands/downloader.js
@@ -301,7 +301,7 @@ cmd({
             if (infoYt.videoDetails.lengthSeconds >= videotime) return citel.reply(`❌ Video file too big!`);
             let titleYt = infoYt.videoDetails.title;
             let randomName = getRandom(".mp3");
-            citel.reply('*│⿻ 🎋⃟🥷 ʏᴏᴜ ꜱᴏɴɢ ᴅᴏᴡɴʟᴏᴅᴀʀ ʙʏ Qᴜᴇᴇɴ ɴɪᴄᴋʏ:* '+titleYt)
+            citel.reply('*│⿻ 🎋⃟🥷\n\n ʏᴏᴜ ꜱᴏɴɢ ᴅᴏᴡɴʟᴏᴅᴀʀ ʙʏ Qᴜᴇᴇɴ ɴɪᴄᴋʏ:* '+titleYt)
             const stream = ytdl(anu.url, {
                     filter: (info) => info.audioBitrate == 160 || info.audioBitrate == 128,
                 })
