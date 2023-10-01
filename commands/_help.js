6d873b365fac75234f58df4909a24799a0e8ba92
QUEENNICKYMDv1 <143050358+QUEENNICKYMDv1@users.noreply.github.com>
2023-10-01 16:48:01 +0530

    Update _help.js

--- commands/_help.js
+++ commands/_help.js
@@ -24,7 +24,7 @@ Secktor.cmd({
             alias: ["menu"],
             desc: "Help list",
             category: "general",
-            react: "🧜",
+            react: "📃",
             filename: __filename
         },
         async(Void, citel, text) => {
