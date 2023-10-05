7264bdbc2f001cf9ec6764bb78c04528b13cbfee
QUEENNICKYMDv1 <143050358+QUEENNICKYMDv1@users.noreply.github.com>
2023-10-05 06:51:56 +0530

    Update downloader.js

--- commands/downloader.js
+++ commands/downloader.js
@@ -95,6 +95,32 @@ cmd(
     }
 );
     //---------------------------------------------------------------------------
+ cmd({
+        pattern: "wallpaper",
+        desc: "To get Random Pics",
+       category: "Anime Pics",
+        filename: __filename
+    },
+
+    async(Suhail, msg, text) => {
+
+
+const response = await fetch('https://api.unsplash.com/photos/random?client_id=72utkjatCBC-PDcx7-Kcvgod7-QOFAm2fXwEeW8b8cc');
+const data = await response.json();
+  const url =data.urls.regular
+                let buttonMessaged = {
+                    image: { url: url },
+                    caption: '*---Random Wallpapers Here---*',
+                    footer: tlang().footer,
+                    headerType: 4,
+                   
+                };
+                return await Suhail.bot.sendMessage(msg.chat, buttonMessaged , {quoted : msg});
+
+
+}
+   )
+ //---------------------------------------------------------------------------
      cmd({
         pattern: "yts",
         desc: "Gives descriptive info of query from youtube..",
