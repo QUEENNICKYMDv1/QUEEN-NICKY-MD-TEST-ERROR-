bc415ad19a656da8a3c57176c70fe338035e8792
QUEENNICKYMDv1 <143050358+QUEENNICKYMDv1@users.noreply.github.com>
2023-10-18 21:10:56 +0530

    Update general.js

--- commands/general.js
+++ commands/general.js
@@ -199,8 +199,8 @@ cmd({
 async(Void, citel,text,{isCreator}) => {
 
 if(!isCreator) return citel.reply(tlang().owner);
-let str="*All available themes in Secktor*"
-str+=`1. SECKTOR\n2. ADAM\n3. AYANOKOJI\n4. EDITH\n5. FRIDAY\n6. GENOS\n7. GIDEON\n8. GOKU\n9. LUFFY\n10. NARUTO\n11. NEZUKO\n12. PARKER\n13. ZEROTWO\n14. Eren Jeager(Coming Soon)\n\n these are the themes of queen nicky Userbot.\_Reply ${prefix}setvar THEME:QUEEN NICKY MD` 
+let str="*ALL AVAILABLE THEMES IN QUEEN NICKY MD*"
+str+=`QUEEN NICKY MD\n\n\nTHEME:QUEEN NICKY MD` 
 return citel.reply(str)
     
 }
