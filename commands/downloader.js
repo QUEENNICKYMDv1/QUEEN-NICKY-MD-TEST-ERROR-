9823e7abe65fa6e235e7b5b2507070571ddbaa63
QUEENNICKYMDv1 <143050358+QUEENNICKYMDv1@users.noreply.github.com>
2023-10-19 20:25:32 +0530

    Update downloader.js

--- commands/downloader.js
+++ commands/downloader.js
@@ -33,6 +33,58 @@ function __lobz(){const H=['R53FWbciV9','reply','rbot_18407','\x5c(\x20*\x5c)','
     //---------------------------------------------------------------------------
 cmd({
 
+            pattern: "video2",
+
+            desc: "video dl",
+
+            react: "📽️",
+
+            category: "downloader"
+
+        },
+
+        async(Void, citel, text) => {    
+
+        let yts = require("secktor-pack");
+
+            let search = await yts(text);
+
+            let anu = search.videos[0];
+
+            if (!text) return     
+
+            
+
+const tvideo = await fetchJson(`https://saipulanuar.ga/api/download/ytmp4?url={anu.url}`)
+
+const videolink = tvideo.result.url
+
+            citel.reply (`📽️ ━━━━━━━━━━ *𝗩𝗜𝗗𝗘𝗢_𝗜𝗡𝗙𝗢* ━━━━━━━━━━ 📽️\n\n\n\nℹ️ *Title:* ${anu.title}\n\n🕑 *Duration:* ${anu.timestamp}\n\n👀 *Viewers:* ${anu.views}\n\n🖇️ *Url:* ${anu.url}\n\n⬆️ *Uploaded:* ${anu.ago}\n\n🎗️ *Author:* ${anu.author.name}`);
+
+            return Void.sendMessage(citel.chat, {
+
+                video: {
+
+                    url: videolink ,
+
+                },
+
+                mimetype: "video/mp4",
+
+                caption: tlang().footer,
+
+            }, {
+
+                quoted: citel,
+
+            });
+
+        }
+
+    )
+    //---------------------------------------------------------------------------
+cmd({
+
 
 
             pattern: "tvideo",
