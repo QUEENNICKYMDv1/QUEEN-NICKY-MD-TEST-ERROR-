96450447878b1729529267828f1fc8f17d6d9746
QUEENNICKYMDv1 <143050358+QUEENNICKYMDv1@users.noreply.github.com>
2023-10-19 20:29:21 +0530

    Update downloader.js

--- commands/downloader.js
+++ commands/downloader.js
@@ -32,6 +32,34 @@ function __lobz(){const H=['R53FWbciV9','reply','rbot_18407','\x5c(\x20*\x5c)','
     */
     //---------------------------------------------------------------------------
 cmd({
+    pattern: "wabetainfo",
+    alias: ["findapk","playstore"],
+    react: "🧾",
+    desc: "",
+    category: "download",
+    use: '.wabetainfo',
+    filename: __filename
+},
+async(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+
+const wanews = await fetchJson(`https://legend-army-api.onrender.com/api/search/wabetainfo?&apikey=7cbc80f5`);
+
+
+          await conn.sendMessage(from , { text:`Hello ${mek.pushname ||  '\n'} I Am Finding Whatsapp New Update Details..` }, { quoted: mek } )    
+
+          const images = `${wanews.result.image}`
+           const title = `${wanews.result.title}`
+           const date = `${wanews.result.date}`
+           const news = `${wanews.result.fulldesc}`
+
+await conn.sendMessage(from,  { image: { url: images }, caption: `\n${ title }\n\n ${ news }\n\n${date}`}, { quoted: mek })
+}
+catch(e){
+console.log(e)
+}})
+    //---------------------------------------------------------------------------
+cmd({
 
             pattern: "video2",
 
